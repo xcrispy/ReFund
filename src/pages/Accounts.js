47719eaafca58c0c0c5c6bc9ab0407e8707503fa
xcrispy <98023462+xcrispy@users.noreply.github.com>
2022-11-03 19:23:14 +0100

    edits

--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -5,9 +5,19 @@ import { useParams } from "react-router-dom";
 export const Account = () => {
   const { account_address } = useParams();
   useEffect(() => {
+    if(account_address.length === 42){
+
     window.location.replace(
       `https://mumbai.polygonscan.com/address/${account_address}`
     );
+          
+  }
   });
-  return <div></div>;
+  return (<div>
+    {account_address.length !== 42 ?
+     <div>
+    <h1>404</h1>
+    </div> 
+    : null}
+  </div>)
 };
